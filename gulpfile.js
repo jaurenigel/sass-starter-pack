bf5c8b911cd3e92f481e7150ab4f51a49ef2c77c
jaurenigel <jaurenigel@gmail.com>
2018-08-22 12:26:43 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+// imports dependencies
+
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create()
+
+// compiling sass
+
+gulp.task('sass', function(){
+    return gulp.src(['src/scss/*.scss'])
+
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream())
+})
+
+// Watch and Serve
+
+gulp.task('serve', ['sass'], function(){
+    browserSync.init({
+        server: './src'
+    })
+
+    gulp.watch(['src/scss/*scss'], ['sass'])
+    gulp.watch(['src/*.html']).on('change', browserSync.reload)
+})
+
+// Default tasks
+
+gulp.task('default', ['serve'])
